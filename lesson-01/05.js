ba87fe676586cfb0674a847ea1911dadb184a6df
MaratAub <super.mario.9@yandex.ru>
2024-01-02 20:15:32 +0300

    test5-3

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,13 +1,36 @@
+let rule = "Еще не родился тот человек, который, поставил бы цель, и не смог бы стать программистом.";
 
-let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
-
-switch (true) { 
-    case rule.length < 25: 
-    console.log("Все таки нет правил без исключения"); 
+switch (rule.length) {
+  case 25:
+    console.log("50 на 50");
     break;
-    case rule.length > 25:
-        console.log("У меня 100 пудов все получится");
-        break;
-        case rule.length = 25:
-            console.log("50 на 50")
+  case 24:
+  case 23:
+  case 22:
+  case 21:
+  case 20:
+  case 19:
+  case 18:
+  case 17:
+  case 16:
+  case 15:
+  case 14:
+  case 13:
+  case 12:
+  case 11:
+  case 10:
+  case 9:
+  case 8:
+  case 7:
+  case 6:
+  case 5:
+  case 4:
+  case 3:
+  case 2:
+  case 1:
+    console.log("Все таки нет правил без исключения");
+    break;
+  default:
+    console.log("У меня 100 пудов все получится");
+    break;
 }
